cb6dfb792b52032ee7d0f06549d9b51d7f3e56b3
Omar Ramos Espinosa <omar.vgdev@gmail.com>
2018-06-05 01:34:00 -0500

    Use of stylesData in SetHome

--- pages/setting_pages/SetHome.js
+++ pages/setting_pages/SetHome.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   Text,
   View,
   Button,
   Platform /* , Alert, */
-} from 'react-native';
-import ActionBar from 'react-native-action-bar';
+} from "react-native";
+import ActionBar from "react-native-action-bar";
 // import PropTypes from 'prop-types';
 // import {StackNavigator,} from 'react-navigation';
 // import RNExitApp from 'react-native-exit-app';
-import Bottom from '../tab_pages/Bottom.js';
+import Bottom from "../tab_pages/Bottom.js";
+
+import { languageDataCtx } from "../../App";
 
 // todo data: take from AppData.json
-const txtToCalibrate = '1. Calibrate Compass';
-const txtToAutoUpdate = '2. Auto Update when pointing';
-const txtToVoiceAssist = '3. Voice Assistant (Disabled)';
-const txtToPreferences = '4. Preferences';
-const txtToProfile = '5. Profile';
+const txtToCalibrate = "1. Calibrate Compass";
+const txtToAutoUpdate = "2. Auto Update when pointing";
+const txtToVoiceAssist = "3. Voice Assistant (Disabled)";
+const txtToPreferences = "4. Preferences";
+const txtToProfile = "5. Profile";
 
-const txtDone = 'Done';
+const txtDone = "Done";
 
 // todo data: take from stylesData
-const colorButtonShadow = '#181818';
-
+const colorButtonShadow = "#181818";
 
 export default class SetHome extends Component<{}> {
   constructor(props) {
@@ -33,67 +34,77 @@ export default class SetHome extends Component<{}> {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View style={styles.mainContainer}>
-        <ActionBar
-          containerStyle={styles.actionBarContainer}
-          titleStyle={styles.actionTitle}
-          title={'noam'}
-          leftIconName={'location'}
-          onLeftPress={() => console.log('Left!')}
-        />
-        <View style={[styles.contentContainer, styles.buttonContainer]}>
-          <Text style={styles.titleText}>Settings</Text>
+      <languageDataCtx.Consumer>
+        {({ stylesData }) => {
+          console.log("stylesData:", stylesData);
+          return (
+            <View style={styles.mainContainer}>
+              <ActionBar
+                containerStyle={styles.actionBarContainer}
+                titleStyle={styles.actionTitle}
+                title={"noam"}
+                leftIconName={"location"}
+                onLeftPress={() => console.log("Left!")}
+              />
+              <View style={[styles.contentContainer, styles.buttonContainer]}>
+                <Text style={styles.titleText}>Settings</Text>
 
-          <View style={styles.buttonDecorator}>
-            <Button
-              onPress={() => navigate('Calibrate')}
-              title={txtToCalibrate}
-              color="#111111"
-              accessibilityLabel="Tap to calibrate compass"
-            />
-          </View>
-          <View style={styles.buttonDecorator}>
-            <Button
-              onPress={() => navigate('AutoUpdate')}
-              title={txtToAutoUpdate}
-              color="#111111"
-              accessibilityLabel="Tap for auto-update settings"
-            />
-          </View>
-          <View style={styles.buttonDecorator}>
-            <Button
-              onPress={() => navigate('Voice')}
-              title={txtToVoiceAssist}
-              color="#111111"
-              accessibilityLabel="Tap on Me"
-            />
-          </View>
-          <View style={styles.buttonDecorator}>
-            <Button
-              onPress={() => navigate('Preferences')}
-              title={txtToPreferences}
-              color="#111111"
-              accessibilityLabel="Tap on Me"
-            />
-          </View>
-          <View style={styles.buttonDecorator}>
-            <Button
-              onPress={() => navigate('Profile')}
-              title={txtToProfile}
-              color="#111111"
-              accessibilityLabel="Tap on Me"
-            />
-          </View>
-        </View>
-        <View style={styles.bottomNavRow}>
-          <Text onPress={() => navigate('MainPage')} style={styles.navButton}>
-            {txtDone}
-          </Text>
-        </View>
-        <View style={styles.bottomRow}>
-          <Bottom />
-        </View>
-      </View>
+                <View style={stylesData.styles.buttonDecorator}>
+                  <Button
+                    onPress={() => navigate("Calibrate")}
+                    title={txtToCalibrate}
+                    color="#111111"
+                    accessibilityLabel="Tap to calibrate compass"
+                  />
+                </View>
+                <View style={stylesData.styles.buttonDecorator}>
+                  <Button
+                    onPress={() => navigate("AutoUpdate")}
+                    title={txtToAutoUpdate}
+                    color="#111111"
+                    accessibilityLabel="Tap for auto-update settings"
+                  />
+                </View>
+                <View style={stylesData.styles.buttonDecorator}>
+                  <Button
+                    onPress={() => navigate("Voice")}
+                    title={txtToVoiceAssist}
+                    color="#111111"
+                    accessibilityLabel="Tap on Me"
+                  />
+                </View>
+                <View style={stylesData.styles.buttonDecorator}>
+                  <Button
+                    onPress={() => navigate("Preferences")}
+                    title={txtToPreferences}
+                    color="#111111"
+                    accessibilityLabel="Tap on Me"
+                  />
+                </View>
+                <View style={stylesData.styles.buttonDecorator}>
+                  <Button
+                    onPress={() => navigate("Profile")}
+                    title={txtToProfile}
+                    color="#111111"
+                    accessibilityLabel="Tap on Me"
+                  />
+                </View>
+              </View>
+              <View style={styles.bottomNavRow}>
+                <Text
+                  onPress={() => navigate("MainPage")}
+                  style={styles.navButton}
+                >
+                  {txtDone}
+                </Text>
+              </View>
+              <View style={styles.bottomRow}>
+                <Bottom />
+              </View>
+            </View>
+          );
+        }}
+      </languageDataCtx.Consumer>
     );
   }
 }
@@ -101,14 +112,14 @@ export default class SetHome extends Component<{}> {
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    marginTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight,
-    backgroundColor: '#FDFDFD'
+    marginTop: Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
+    backgroundColor: "#FDFDFD"
   },
   actionBarContainer: {
-    backgroundColor: '#330077'
+    backgroundColor: "#330077"
   },
   actionTitle: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 20
   },
   contentContainer: {
@@ -121,15 +132,15 @@ const styles = StyleSheet.create({
     marginRight: 20,
     marginBottom: 10
   },
-  buttonContainer:{
-    flexDirection: 'column',
-    justifyContent: 'space-between'
+  buttonContainer: {
+    flexDirection: "column",
+    justifyContent: "space-between"
   },
   buttonDecorator: {
-    backgroundColor: '#444444', 
+    backgroundColor: "#444444",
     borderRadius: 10,
     padding: 3,
-    shadowColor: colorButtonShadow, 
+    shadowColor: colorButtonShadow,
     shadowOffset: {
       width: 0,
       height: 1
@@ -141,12 +152,12 @@ const styles = StyleSheet.create({
   },
   bottomNavRow: {
     flex: 2,
-    flexDirection: 'row',
+    flexDirection: "row",
     marginLeft: 15,
-    justifyContent: 'space-between'
+    justifyContent: "space-between"
   },
   navButton: {
-    textDecorationLine: 'underline',
+    textDecorationLine: "underline",
     fontSize: 22
   },
   bottomRow: {
