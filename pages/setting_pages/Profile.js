cca8a9a6d4c97c95ce030fedc06059b83c5c4530
pashute <pashute@gmail.com>
2018-06-11 18:49:32 +0300

    fixed  profile style mistakes

--- pages/setting_pages/Profile.js
+++ pages/setting_pages/Profile.js
@@ -31,14 +31,14 @@ export default class Profile extends Component<{}> {
         this.setState({ username: value });
       }
     });
-  
+
     SecureStore.getItemAsync('preferences-email').then(value => {
       // console.log('dbg.profile secureStore.email', value);
       if (value !== null) {
         this.setState({ email: value });
       }
     });
-  
+
     SecureStore.getItemAsync('preferences-password').then(value => {
       // console.log('dbg.profile secureStore.password', value);
       if (value !== null) {
@@ -46,91 +46,116 @@ export default class Profile extends Component<{}> {
       }
     });
   }
-  
 
   render() {
     const { navigate, goBack, replace, popToTop } = this.props.navigation;
     return (
       <languageDataCtx.Consumer>
         {({ stylesData, appData }) => {
-          return(
-            <View style={[
-              stylesData.styles.sharedStyles.mainContainer,
-              styles.topMargin
-            ]}>
+          return (
+            <View
+              style={[
+                stylesData.styles.sharedStyles.mainContainer,
+                styles.topMargin
+              ]}
+            >
               <ActionBar
-                containerStyle={stylesData.styles.sharedStyles.actionBarContainer}
+                containerStyle={
+                  stylesData.styles.sharedStyles.actionBarContainer
+                }
                 titleStyle={stylesData.styles.sharedStyles.actionTitle}
                 title={appData.appData.general.txtAppNameOnActionBar}
                 leftIconName={'location'}
-                onLeftPress={() => console.log('dbg.pref.actionbar requested Voice-Assist!')}
+                onLeftPress={() =>
+                  console.log('dbg.pref.actionbar requested Voice-Assist!')
+                }
               />
               <View style={stylesData.styles.sharedStyles.contentContainer}>
-                <Text style={stylesData.styles.sharedStyles.titleText}>{appData.appData.general.txtSettings}</Text>
-                <Text style={stylesData.styles.sharedStyles.instructionsHeader}>{appData.appData.screensSettings.profile.txtTitle}</Text>
-                <View style={stylesData.styles.sharedStyles.insturctionsView}>
-                  <Text style={stylesData.styles.sharedStyles.instructions}>{appData.appData.screensSettings.profile.txtInstrcutions}</Text>
-                  <View style={styles.inputsView}>
+                <Text style={stylesData.styles.sharedStyles.titleText}>
+                  {appData.appData.general.txtSettings}
+                </Text>
+                <Text style={stylesData.styles.sharedStyles.instructionsHeader}>
+                  {appData.appData.screensSettings.profile.txtTitle}
+                </Text>
+                <View style={stylesData.styles.sharedStyles.instructionsView}>
+                  <Text style={stylesData.styles.sharedStyles.instructions}>
+                    {appData.appData.screensSettings.profile.txtInstructions}
+                  </Text>
+                  <View style={stylesData.styles.sharedStyles.inputsView}>
                     <TextInput
                       style={stylesData.styles.sharedStyles.inputText}
                       value={this.state.username}
-                      placeholder="Username"
+                      placeholder={
+                        appData.appData.screensSettings.profile.txtUsername
+                      }
                       onChangeText={text => {
-                        SecureStore.setItemAsync('preferences-username', text.toString());
-                        this.setState({ username: text })
+                        SecureStore.setItemAsync(
+                          'preferences-username',
+                          text.toString()
+                        );
+                        this.setState({ username: text });
                       }}
                     />
                     <TextInput
-                      style={{ height: 40, fontSize: 25 }}
+                      style={stylesData.styles.sharedStyles.inputText}
                       value={this.state.email}
-                      placeholder="email"
+                      placeholder={
+                        appData.appData.screensSettings.profile.txtEmail
+                      }
                       onChangeText={text => {
-                        SecureStore.setItemAsync('preferences-email', text.toString());
-                        this.setState({ email: text })
+                        SecureStore.setItemAsync(
+                          'preferences-email',
+                          text.toString()
+                        );
+                        this.setState({ email: text });
                       }}
                     />
                     <TextInput
-                      style={{ height: 40, fontSize: 25 }}
+                      style={stylesData.styles.sharedStyles.inputText}
                       value={this.state.password}
                       secureTextEntry={true}
-                      placeholder="password"
+                      placeholder={
+                        appData.appData.screensSettings.profile.txtPassword
+                      }
                       onChangeText={text => {
-                        SecureStore.setItemAsync('preferences-password', text.toString());
-                        this.setState({ password: text })
+                        SecureStore.setItemAsync(
+                          'preferences-password',
+                          text.toString()
+                        );
+                        this.setState({ password: text });
                       }}
                     />
-                    <View style={styles.separator60} />
                   </View>
                 </View>
-                <View style={stylesData.styles.sharedStyles.bottomNavRow}>
-                  <Text
-                    onPress={() => {
-                      navigate('Preferences');
-                    }}
-                    style={stylesData.styles.sharedStyles.navButton}
-                  >
-                    {appData.appData.general.txtBack}
-                  </Text>
-                  <Text
-                    onPress={() => {
-                      navigate('SetHome');
-                    }}
-                    style={stylesData.styles.sharedStyles.navButton}
-                  >
-                    {appData.appData.general.txtTOC}
-                  </Text>
-                  <Text
-                    onPress={() => {
-                      navigate('MainPage');
-                    }}
-                    style={stylesData.styles.sharedStyles.navButton}
-                  >
-                    {appData.appData.general.txtDone}
-                  </Text>
-                </View>
-                <View style={stylesData.styles.sharedStyles.bottomRow}>
-                  <Bottom />
-                </View>
+              </View>
+              <View style={stylesData.styles.sharedStyles.bottomNavRow}>
+                <Text
+                  onPress={() => {
+                    navigate('Preferences');
+                  }}
+                  style={stylesData.styles.sharedStyles.navButton}
+                >
+                  {appData.appData.general.txtBack}
+                </Text>
+                <Text
+                  onPress={() => {
+                    navigate('SetHome');
+                  }}
+                  style={stylesData.styles.sharedStyles.navButton}
+                >
+                  {appData.appData.general.txtTOC}
+                </Text>
+                <Text
+                  onPress={() => {
+                    navigate('MainPage');
+                  }}
+                  style={stylesData.styles.sharedStyles.navButton}
+                >
+                  {appData.appData.general.txtDone}
+                </Text>
+              </View>
+              <View style={stylesData.styles.sharedStyles.bottomRow}>
+                <Bottom />
               </View>
             </View>
           );
