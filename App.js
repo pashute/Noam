da4ceaf829e0f3de1f6e030f6bd84a4305eaf20c
pashute <pashute@gmail.com>
2018-06-15 02:35:50 +0300

    no need for AppSwitch

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
-// Export the one you want
-import AppMain from './AppMain'; // Main-App
-import AppStorybook from './storybook'; // Storybook view
-import AppSwitch from './AppSwitch'; // Dev: storybook, Prod: Main-App
+// This will load StorybookUI on Dev and App in Release
+// import { module } from 'react-native';
+import StorybookUI from './storybook';
+import AppMain from './AppMain';
 
-export default AppSwitch;
+module.exports = __DEV__ ? StorybookUI : AppMain;
+
+// // Export the one you want
+// import AppMain from './AppMain'; // Main-App
+// import AppStorybook from './storybook'; // Storybook view
+// import AppSwitch from './AppSwitch'; // Dev: storybook, Prod: Main-App
+
+// export default AppSwitch;
