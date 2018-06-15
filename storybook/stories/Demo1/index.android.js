865cd4ba20f908e7b6bc0189dfec379bf87d1b3a
pashute <pashute@gmail.com>
2018-06-15 03:17:44 +0300

    fixed storybook demo

--- storybook/stories/Demo1/index.android.js
+++ storybook/stories/Demo1/index.android.js
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { TouchableNativeFeedback } from 'react-native';
 
-const Button = props => (
+const Demo1 = props => (
   <TouchableNativeFeedback onPress={props.onPress}>{props.children}</TouchableNativeFeedback>
 );
 
-Button.propTypes = {
+Demo1.propTypes = {
   children: PropTypes.node.isRequired,
   onPress: PropTypes.func,
 };
-Button.defaultProps = {
+Demo1.defaultProps = {
   onPress: () => {},
 };
 
-export { Button as default };
+export { Demo1 as default };
--- storybook/stories/Demo1/index.ios.js
+++ storybook/stories/Demo1/index.ios.js
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { TouchableHighlight } from 'react-native';
 
-const Button = props => (
+const Demo1 = props => (
   <TouchableHighlight onPress={props.onPress}>{props.children}</TouchableHighlight>
 );
 
-Button.propTypes = {
+Demo1.propTypes = {
   children: PropTypes.node.isRequired,
   onPress: PropTypes.func,
 };
-Button.defaultProps = {
+Demo1.defaultProps = {
   onPress: () => {},
 };
 
-export { Button as default };
+export { Demo1 as default };
