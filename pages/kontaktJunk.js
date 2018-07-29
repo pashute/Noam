3e9ed572c6f93a2b67d49997ac66356fde9580f7
karan <cooldownkb@gmail.com>
2018-07-30 00:46:47 +0530

    commit

--- pages/kontaktJunk.js
+++ pages/kontaktJunk.js
@@ -66,6 +66,9 @@ const region1 = {
  * sorted by their RSSI which reflects a measure of distance from the beacons
  * to your mobile phone.
  */
+
+//
+
 export default class KontakIoAndroid extends Component {
     state = {
         scanning: false,
