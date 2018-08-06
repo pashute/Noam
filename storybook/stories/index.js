572995293590d6abb63f430faa27f3b4ec28b98f
pashute <pashute@gmail.com>
2018-08-06 15:39:09 +0300

    took mainsplash and stories index.js from storySpecs

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { Text } from 'react-native';
+//x import { Text } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+//x import { action } from '@storybook/addon-actions';
+//x import { linkTo } from '@storybook/addon-links';
 
-import MainSplash from 'storybook/stories/MainSplash';
-// Moshe: if we had time, I would rename mainSlash to SlashStory
-// something like this. need more time. so later.
-storiesOf('splashStory', module).add('to Storybook', () => (
-  <MainSplash />
-));
+//x import CenterView from './CenterView';
+import MainSplash from './MainSplash';
+import MainInPlace from './MainInPlace';
+import MainWay from './MainWay';
+import MainNearby from './MainNearby';
 
+storiesOf('Noam', module)
+  .add('Splash', () => (<MainSplash />))
+  .add('Main tabs', () => (<MainInPlace />));
 
+// storiesOf('Belcome', module).add('to Storybook', () => (
+//   <Demo0 showApp={linkTo('Button')} />
+// ));
 
+// storiesOf('Demo1', module)
+//   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
+//   .add('with text', () => (
+//     <Demo1 onPress={action('clicked-text')}>
+//       <Text>Hello Button</Text>
+//     </Demo1>
+//   ))
+//   .add('with some emoji', () => (
+//     <Demo1 onPress={action('clicked-emoji')}>
+//       <Text>😀 😎 👍 💯</Text>
+//     </Demo1>
+//   ));
