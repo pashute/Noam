5cc298fa8716d7fdfa7a8106050b8784af62eba1
pashute <pashute@gmail.com>
2018-08-14 05:02:13 +0300

    dbg console msg (dataActions.js)

--- redux/actions/DataActions.js
+++ redux/actions/DataActions.js
@@ -3,7 +3,7 @@ import * as types from '../types';
 import { AsyncStorage } from 'react-native';
 
 export const setCurrentLanguage = language => {
-  console.log(language);
+  console.log('dbg.dataActions.language', language);
   return {
     type: types.SET_CURRENT_LANGUAGE,
     language: language
